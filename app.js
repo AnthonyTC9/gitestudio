1b0311272ed6066c1d38df0932abe92bf598b791
Intern 1 <intern1@iMac-de-Eden.local>
2022-07-21 15:44:03 -0600

    Agregamos codigo de node.js en el archivo app.js y editamos index.html con JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log(`hola me falto este mensaje`);
+const http = require("http");
+
+function sitioweb(req,res)
+{
+    res.writeHead(200, {"Content-type": "text/html"});
+    res.end("Mi <strong>PRIMER</strong> servidor");
+}
+
+let servidor = http.createServer(sitioweb);
+
+servidor.listen(6969, "127.0.0.1");
+
+console.log("servidor corriendo en el puerto 6969");
